9155f24976e8ec64f15ae8e6304d4f876d4b519b
Seungmin <rnsdls1997@gmail.com>
2025-10-31 17:29:18 +0900

    refactor: 컴íComponentSãcan 재귀 형식으로 탐색

--- src/main/java/org/example/framework/core/ComponentScanner.java
+++ src/main/java/org/example/framework/core/ComponentScanner.java
@@ -5,60 +5,61 @@ import org.example.framework.annotation.Component;
 import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
+/**
+ * 지정된 패키지 경로에서 @Component 어노테이션이 붙은 클래스를 탐색하는 유틸리티
+ * TODO: @ComponentScan(basePackage={"org.example.app"}) 방식의 어노테이션 기반으로 바꾸기
+ */
 public class ComponentScanner {
-    private final List<String> basePackage;
 
-    public ComponentScanner() {
-        // 스캔할 패키지 등록
-        basePackage = new ArrayList<>();
-        basePackage.add("org/example/app");
-        basePackage.add("org/example/framework/web");
+    private final List<String> basePackages;
+
+    /**
+     * 스캔할 패키지 목록을 생성자에서 주입받는다.
+     * ex) new ComponentScanner("org.example.app", "org.example.framework.web")
+     */
+    public ComponentScanner(String... packages) {
+        this.basePackages = List.of(packages);
     }
 
+    /**
+     * 지정된 모든 패키지를 순회하며 @Component 클래스들을 수집
+     */
     public Set<Class<?>> scan() throws URISyntaxException, ClassNotFoundException {
         Set<Class<?>> componentSet = new HashSet<>();
-        // basePackage 순회
-        for(String basePackageUrl : basePackage) {
-            // basePackage 기반으로 클래스 패스 가져오기
-            URL url = ClassLoader.getSystemClassLoader().getResource(basePackageUrl);
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 
-            if(url != null) {
-                File dir = new File(url.toURI());
-                File[] classFile = dir.listFiles(file -> file.getName().endsWith(".class"));
+        for (String basePackage : basePackages) {
+            String path = basePackage.replace('.', '/');
+            URL url = classLoader.getResource(path);
+            if (url == null) continue;
 
-                if(classFile == null)
-                    throw new ClassNotFoundException("클래스 파일을 읽을 수 없습니다." + dir.getPath());
-
-                for(File file : classFile) {
-                    String className = getClassName(file);
-
-                    // 클래스 가져오기
-                    Class<?> clazz = Class.forName(className);
-
-                    if(clazz.isAnnotationPresent(Component.class))
-                        componentSet.add(clazz);
-                }
-            }
+            File dir = new File(url.toURI());
+            collectClasses(dir, basePackage, componentSet, classLoader);
         }
         return componentSet;
     }
 
-    private String getClassName(File file) {
-        String fullPath = file.getAbsolutePath();
-        // 상대 경로 추출
-        String basePath = new File("build/classes/java/main").getAbsolutePath();
-        String relativePath = fullPath.substring(basePath.length() + 1);
-
-        // 확장자 제거
-        String className = relativePath.replace(".class", "");
-
-        // 경로 구분 -> 패키지 구분
-        return className.replace(File.separatorChar, '.');
+    /**
+     * 디렉터리 내 .class 파일을 재귀적으로 탐색하고, @Component 클래스만 등록
+     */
+    private void collectClasses(File dir, String packageName, Set<Class<?>> components, ClassLoader loader)
+            throws ClassNotFoundException {
+        if (!dir.exists()) return;
+        for (File file : Objects.requireNonNull(dir.listFiles())) {
+            if (file.isDirectory()) {
+                collectClasses(file, packageName + "." + file.getName(), components, loader);
+            } else if (file.getName().endsWith(".class")) {
+                String className = packageName + "." + file.getName().replace(".class", "");
+                Class<?> clazz = loader.loadClass(className);
+                if (clazz.isAnnotationPresent(Component.class)) {
+                    components.add(clazz);
+                }
+            }
+        }
     }
-
 }
