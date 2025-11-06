feacc9b56447fa2d4a934e32a8d72bfd73ff0dde
Seungmin <rnsdls1997@gmail.com>
2025-11-07 00:14:29 +0900

    feat: cause 포함 생성자 추가

--- src/main/java/org/example/framework/exception/bean/BeanCreationException.java
+++ src/main/java/org/example/framework/exception/bean/BeanCreationException.java
@@ -1,7 +1,13 @@
 package org.example.framework.exception.bean;
 
-public class BeanCreationException extends BeanException{
+public class BeanCreationException extends BeanException {
+    private static final String PREFIX = "Failed to create bean: ";
+
     public BeanCreationException(String beanName) {
-        super("Failed to create bean: " + beanName);
+        super(PREFIX + beanName);
+    }
+
+    public BeanCreationException(String beanName, Throwable e) {
+        super(PREFIX + beanName, e);
     }
 }
--- src/main/java/org/example/framework/exception/bean/NoSuchBeanDefinitionException.java
+++ src/main/java/org/example/framework/exception/bean/NoSuchBeanDefinitionException.java
@@ -1,7 +1,13 @@
 package org.example.framework.exception.bean;
 
-public class NoSuchBeanDefinitionException extends BeanException{
+public class NoSuchBeanDefinitionException extends BeanException {
+    private static final String PREFIX = "No BeanDefinition found for bean name: ";
+
     public NoSuchBeanDefinitionException(String beanName) {
-        super("No BeanDefinition found for bean name: " + beanName);
+        super(PREFIX + beanName);
+    }
+
+    public NoSuchBeanDefinitionException(String beanName, Throwable cause) {
+        super(PREFIX + beanName, cause);
     }
 }
