8a2fa39fbf024283fc6458908a08d203605628d8
Seungmin <rnsdls1997@gmail.com>
2025-11-07 00:55:24 +0900

    feat: MyBeanFactory 추가

---
+++ src/main/java/org/example/framework/context/MyBeanFactory.java
@@ -0,0 +1,194 @@
+package org.example.framework.context;
+
+import org.example.framework.core.BeanDefinitionRegistry;
+import org.example.framework.core.BeanFactory;
+import org.example.framework.core.DependencyInjector;
+import org.example.framework.exception.bean.BeanCreationException;
+import org.example.framework.exception.bean.NoSuchBeanDefinitionException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyBeanFactory implements BeanFactory {
+
+    private final DependencyInjector injector;
+    private final BeanDefinitionRegistry registry;
+
+    /** Cache of singleton objects: bean name --> bean instance */
+    private final Map<String, Object> singletonObjects = new HashMap<>();
+
+    public MyBeanFactory(DependencyInjector injector, BeanDefinitionRegistry registry) {
+        this.injector = injector;
+        this.registry = registry;
+    }
+
+    /**
+     * Bean이 존재하는지 확인<br>
+     * {@link MyBeanFactory}의 {@code singletonObjects}와 {@link MyBeanDefinitionRegistry}의 {@code beanDefinitions}를 확인한다.
+     * @param name 확인할 Bean 이름
+     * @return {@code boolean}
+     */
+    @Override
+    public boolean containsBean(String name) {
+        return containsSingleton(name) || registry.containsBeanDefinition(name);
+    }
+
+
+    @Override
+    public String[] getAliases(String name) {
+        //TODO: 추후 alias 개발하면 구현 예정
+        return new String[0];
+    }
+
+    /**
+     * 주어진 타입과 일치하거나 호환되는 Bean을 반환한다.
+     * <p>싱글톤 캐시에서 먼저 탐색하고, 없을 경우 등록된 BeanDefinition 중
+     * 지정된 타입과 호환되는 Bean을 찾아 생성한다.</p>
+     *
+     * @param requiredType 찾을 Bean의 타입
+     * @param <T> 반환 타입
+     * @return 해당 타입의 Bean 인스턴스
+     * @throws NoSuchBeanDefinitionException 일치하는 Bean 정의가 존재하지 않는 경우
+     */
+    @Override
+    public <T> T getBean(Class<T> requiredType) {
+        // 1. 싱글톤 캐시 검색
+        for(Object obj : singletonObjects.values()) {
+            if(requiredType.isInstance(obj))
+                return requiredType.cast(obj);
+        }
+
+        // 2. BeanDefinition 검색 및 생성
+        for (BeanDefinition def : registry.getBeanDefinitions()) {
+            if (requiredType.isAssignableFrom(def.getBeanClass())) {
+                Object instance = getBean(def.getBeanName());
+                return requiredType.cast(instance);
+            }
+        }
+
+        throw new NoSuchBeanDefinitionException(requiredType.getSimpleName());
+    }
+
+    /**
+     * 지정된 이름의 Bean을 반환한다.
+     * <p>싱글톤 캐시에 존재하면 재사용하고, 없으면 새로 생성하여 필요 시 캐시에 등록한다.</p>
+     *
+     * @param beanName 조회할 Bean의 이름
+     * @return Bean 인스턴스
+     * @throws NoSuchBeanDefinitionException 정의되지 않은 Bean 이름인 경우
+     */
+    @Override
+    public Object getBean(String beanName) {
+        if(containsSingleton(beanName))
+            return singletonObjects.get(beanName);
+
+        // 등록된 Bean 이 없으면 생성
+        BeanDefinition beanDefinition = getBeanDefinitionOrThrow(beanName);
+        Object obj = createBean(beanDefinition);
+
+        if(beanDefinition.isSingleton())
+            singletonObjects.put(beanName, obj);
+
+        return obj;
+    }
+
+    /**
+     * 지정된 Bean 이름에 해당하는 타입을 반환한다.<br>
+     * 이미 생성된 Bean 인스턴스 또는 {@link BeanDefinition} 메타정보를 기반으로
+     * Bean의 타입을 판별한다.
+     * @param name 조회할 Bean 이름
+     * @return Bean의 타입 (Class)
+     * @throws NoSuchBeanDefinitionException 지정된 이름의 {@link BeanDefinition}이 존재하지 않을 때
+     */
+    @Override
+    public Class<?> getType(String name) {
+        if(containsSingleton(name))
+            return singletonObjects.get(name).getClass();
+
+        return getBeanDefinitionOrThrow(name).getBeanClass();
+    }
+
+    /**
+     * 지정된 Bean의 {@link ScopeType}이 {@code prototype}인지 확인한다.<br>
+     * {@link BeanDefinition}의 정보를 조회한다.
+     * @param name 조회할 Bean 이름
+     * @return {@code prototype} 여부
+     * @throws NoSuchBeanDefinitionException 지정된 이름의 {@link BeanDefinition}이 존재하지 않을 때
+     */
+    @Override
+    public boolean isPrototype(String name) {
+        return getBeanDefinitionOrThrow(name).isPrototype();
+    }
+
+    /**
+     * 지정된 Bean의 {@link ScopeType}이 {@code singleton}인지 확인한다.<br>
+     * {@link BeanDefinition}의 정보를 조회한다.
+     * @param name 조회할 Bean 이름
+     * @return {@code singleton} 여부
+     * @throws NoSuchBeanDefinitionException 지정된 이름의 {@link BeanDefinition}이 존재하지 않을 때
+     */
+    @Override
+    public boolean isSingleton(String name) {
+        return getBeanDefinitionOrThrow(name).isSingleton();
+    }
+
+    /**
+     * 지정된 이름의 Bean이 주어진 타입과 호환되는지 확인한다.
+     *
+     * <p>등록된 {@link BeanDefinition}의 타입이 typeToMatch와
+     * 동일하거나 하위 타입(또는 구현 클래스)일 경우 true를 반환한다.</p>
+     *
+     * @param name        검사할 Bean의 이름
+     * @param typeToMatch 비교할 대상 타입
+     * @return 타입이 일치하거나 호환되면 true, 그렇지 않으면 false
+     * @throws NoSuchBeanDefinitionException Bean 정의가 존재하지 않을 경우
+     */
+    @Override
+    public boolean isTypeMath(String name, Class<?> typeToMatch) {
+        BeanDefinition beanDefinition = getBeanDefinitionOrThrow(name);
+        return typeToMatch.isAssignableFrom(beanDefinition.getBeanClass());
+    }
+
+    /**
+     * {@code singletonObjects} 안에 해당 Bean이 존재하는지 검사
+     */
+    private boolean containsSingleton(String name) {
+        return singletonObjects.containsKey(name);
+    }
+
+    /**
+     * 지정된 Bean의 존재 여부를 검사한다.<br>
+     * {@link BeanDefinitionRegistry}의 {@code beanDefinitions}를 조회한다.
+     * @param name 조회할 Bean의 이름
+     * @throws NoSuchBeanDefinitionException 지정된 이름의 {@link BeanDefinition}이 존재하지 않을 때
+     */
+    private BeanDefinition getBeanDefinitionOrThrow(String name) {
+        if(!registry.containsBeanDefinition(name))
+            throw new NoSuchBeanDefinitionException(name);
+        return registry.getBeanDefinition(name);
+    }
+
+    /**
+     * 주어진 {@link BeanDefinition} 정보를 기반으로 새로운 Bean 인스턴스를 생성한다.
+     *
+     * <p>기본 생성자를 통해 객체를 생성하며, 이후 {@code injector}를 이용해
+     * 필요한 의존성을 주입한다. 생성 과정에서 발생한 예외는
+     * {@link BeanCreationException}으로 감싸서 던진다.</p>
+     *
+     * @param def Bean 정의 정보
+     * @return 생성된 Bean 인스턴스
+     * @throws BeanCreationException Bean 생성 중 예외가 발생한 경우
+     */
+    private Object createBean(BeanDefinition def) {
+        try {
+            Class<?> clazz = def.getBeanClass();
+            Object instance = clazz.getDeclaredConstructor().newInstance();
+
+            // 의존성 주입
+            injector.inject(instance, this);
+            return instance;
+        } catch (Exception e) {
+            throw new BeanCreationException(def.getBeanName(), e);
+        }
+    }
+}
