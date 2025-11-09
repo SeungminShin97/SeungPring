3145d25277155bbc0aca37bbb377567c426b4fb0
Seungmin <rnsdls1997@gmail.com>
2025-11-09 17:31:33 +0900

    feat: 생성자 오버로딩
    - BeanDefinition(Class<?>)
    - BeanDefinition(Class<?>, String)
    - BeanDefinition(Class<?>, ScopeType)

--- src/main/java/org/example/framework/context/BeanDefinition.java
+++ src/main/java/org/example/framework/context/BeanDefinition.java
@@ -1,9 +1,23 @@
 package org.example.framework.context;
 
+import java.beans.Introspector;
+
 public class BeanDefinition {
     private final Class<?> beanClass;
     private final ScopeType scope;
     private final String beanName;
+
+    public BeanDefinition(Class<?> clazz) {
+        this(clazz, Introspector.decapitalize(clazz.getSimpleName()), ScopeType.SINGLETON);
+    }
+
+    public BeanDefinition(Class<?> clazz, String beanName) {
+        this(clazz, beanName, ScopeType.SINGLETON);
+    }
+
+    public BeanDefinition(Class<?> clazz, ScopeType scope) {
+        this(clazz, Introspector.decapitalize(clazz.getSimpleName()), scope);
+    }
 
     public BeanDefinition(Class<?> clazz, String beanName, ScopeType scopeType) {
         this.beanClass = clazz;
