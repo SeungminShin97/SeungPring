cf69f43117e539d01cf400fe3a404da31c877ea4
Seungmin <rnsdls1997@gmail.com>
2025-11-13 20:24:10 +0900

    refactor: HttpProtocolHandler 상속으로 변경

--- src/main/java/org/example/framework/was/protocol/http/Http1ProtocolHandler.java
+++ src/main/java/org/example/framework/was/protocol/http/Http1ProtocolHandler.java
@@ -3,11 +3,17 @@ package org.example.framework.was.protocol.http;
 import org.example.framework.exception.todo.TODOCode;
 import org.example.framework.exception.todo.TODOException;
 import org.example.framework.was.protocol.core.HttpProtocolHandler;
+import org.example.framework.was.protocol.core.RequestParser;
+import org.example.framework.was.protocol.core.ResponseWriter;
 
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class Http1ProtocolHandler implements HttpProtocolHandler {
+public class Http1ProtocolHandler extends HttpProtocolHandler {
+
+    public Http1ProtocolHandler(RequestParser requestParser, ResponseWriter responseWriter) {
+        super(requestParser, responseWriter);
+    }
 
     @Override
     public void process(InputStream inputStream, OutputStream outputStream) {
--- src/main/java/org/example/framework/was/protocol/http/Http2ProtocolHandler.java
+++ src/main/java/org/example/framework/was/protocol/http/Http2ProtocolHandler.java
@@ -3,11 +3,18 @@ package org.example.framework.was.protocol.http;
 import org.example.framework.exception.todo.TODOCode;
 import org.example.framework.exception.todo.TODOException;
 import org.example.framework.was.protocol.core.HttpProtocolHandler;
+import org.example.framework.was.protocol.core.RequestParser;
+import org.example.framework.was.protocol.core.ResponseWriter;
 
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class Http2ProtocolHandler implements HttpProtocolHandler {
+public class Http2ProtocolHandler extends HttpProtocolHandler {
+
+    public Http2ProtocolHandler(RequestParser requestParser, ResponseWriter responseWriter) {
+        super(requestParser, responseWriter);
+    }
+
     @Override
     public void process(InputStream inputStream, OutputStream outputStream) {
         throw new TODOException(TODOCode.MAIN_HTTP2, TODOCode.SUB_PROTOCOL_HANDLER);
