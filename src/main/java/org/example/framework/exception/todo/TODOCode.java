7e30e5a29c005f04bc4de7c7a78cb6bc383572eb
Seungmin <rnsdls1997@gmail.com>
2025-11-13 20:27:40 +0900

    chore: todo 코드 추가

--- src/main/java/org/example/framework/exception/todo/TODOCode.java
+++ src/main/java/org/example/framework/exception/todo/TODOCode.java
@@ -2,9 +2,11 @@ package org.example.framework.exception.todo;
 
 public enum TODOCode {
     // 대분류
+    MAIN_HTTP1,
     MAIN_HTTP2,
 
     // 소분류
     SUB_PROTOCOL_HANDLER,
-    SUB_REQUEST_PARSER;
+    SUB_REQUEST_PARSER,
+    SUB_RESPONSE_WRITER
 }
