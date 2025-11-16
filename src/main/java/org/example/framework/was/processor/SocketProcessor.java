89c541b37c2164c5294d9710135eec4e01beee0d
Seungmin <rnsdls1997@gmail.com>
2025-11-16 23:25:12 +0900

    refactor: SocketProcessor 구현
    - HttpProtocolSelector, HttpProtocolHandler 외부 주입으로 변경 (테스트 유리)
    - 각 예외별 처리 추가

--- src/main/java/org/example/framework/was/processor/SocketProcessor.java
+++ src/main/java/org/example/framework/was/processor/SocketProcessor.java
@@ -1,22 +1,47 @@
 package org.example.framework.was.processor;
 
+import org.example.framework.exception.was.HttpParsingException;
+import org.example.framework.was.protocol.HttpProtocolSelector;
+import org.example.framework.was.protocol.HttpProtocolVersion;
+import org.example.framework.was.protocol.core.HttpProtocolHandler;
+import org.example.framework.was.protocol.http.HttpProtocolHandlerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class SocketProcessor implements Runnable{
+    private static final Logger log = LoggerFactory.getLogger(SocketProcessor.class);
 
     private final Socket socket;
+    private final HttpProtocolSelector selector;
+    private final HttpProtocolHandlerFactory handlerFactory;
 
-    public SocketProcessor(Socket socket) {
+    public SocketProcessor(Socket socket, HttpProtocolSelector selector, HttpProtocolHandlerFactory handlerFactory) {
         this.socket = socket;
+        this.selector = selector;
+        this.handlerFactory = handlerFactory;
     }
 
     @Override
     public void run() {
         try (socket) {
-            // 요청 처리
-            System.out.println("asdf");
+            InputStream in = socket.getInputStream();
+            OutputStream out = socket.getOutputStream();
+
+            HttpProtocolVersion version = selector.detect(in);
+            HttpProtocolHandler handler = handlerFactory.getHandler(version);
+
+            handler.process(in, out);
         } catch (IOException e) {
+            log.error("[SocketProcessor] Socket I/O Error, connection closed by client/network: {}", e.getMessage());
+        } catch (HttpParsingException e) {
+            // TODO: 400 에러 발송 후 소켓종료 로직 추가
+        } catch (Exception e) {
+            // TODO: 500 예외 발송??
             throw new RuntimeException(e);
         }
     }
