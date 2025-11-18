0095f3db359b9e316af40f965f162d188ee31884
Seungmin <rnsdls1997@gmail.com>
2025-11-18 22:11:29 +0900

    feat: HttpWritingException 예외 추가

---
+++ src/main/java/org/example/framework/exception/was/HttpWritingException.java
@@ -0,0 +1,16 @@
+package org.example.framework.exception.was;
+
+/**
+ * Http 출력에 실패할 경우 던지는 예외 <br>
+ * 잡는 곳에서 500 Internal Server Error 응답으로 변환해야 한다.
+ */
+public class HttpWritingException extends Exception {
+
+    public HttpWritingException(String message) {
+        super(message);
+    }
+
+    public HttpWritingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
