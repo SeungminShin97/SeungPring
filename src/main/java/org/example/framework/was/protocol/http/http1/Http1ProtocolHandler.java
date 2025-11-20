fde76926688077b0263653f256f4cbdd59e2fe2f
Seungmin <rnsdls1997@gmail.com>
2025-11-20 13:42:36 +0900

    refactor: process 메서드 예외 추가 및 writer 구현

--- src/main/java/org/example/framework/was/protocol/http/http1/Http1ProtocolHandler.java
+++ src/main/java/org/example/framework/was/protocol/http/http1/Http1ProtocolHandler.java
@@ -1,12 +1,14 @@
 package org.example.framework.was.protocol.http.http1;
 
-import org.example.framework.exception.todo.TODOCode;
-import org.example.framework.exception.todo.TODOException;
 import org.example.framework.exception.was.HttpParsingException;
+import org.example.framework.exception.was.HttpWritingException;
 import org.example.framework.was.protocol.core.HttpProtocolHandler;
 import org.example.framework.was.protocol.core.RequestParser;
 import org.example.framework.was.protocol.core.ResponseWriter;
+import org.example.framework.was.protocol.model.HttpMessage;
+import org.example.framework.was.protocol.model.HttpResponse;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.UnsupportedCharsetException;
@@ -29,8 +31,10 @@ public class Http1ProtocolHandler extends HttpProtocolHandler {
     }
 
     @Override
-    public void process(InputStream inputStream, OutputStream outputStream) throws HttpParsingException, UnsupportedCharsetException {
-        super.requestParser.parse(inputStream);
-        throw new TODOException(TODOCode.SUB_PROTOCOL_HANDLER);
+    public void process(InputStream inputStream, OutputStream outputStream) throws HttpParsingException, UnsupportedCharsetException, HttpWritingException, IOException {
+        HttpMessage request = super.requestParser.parse(inputStream);
+        //TODO: 디스패처 서블릿 구현 시 교체
+        HttpResponse response = new HttpResponse(null, null, null, 200, null);
+        super.responseWriter.write(outputStream, response);
     }
 }
