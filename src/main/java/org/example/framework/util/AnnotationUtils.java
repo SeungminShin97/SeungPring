7a1a60420028d1e68feb00aa9d943fe36800c280
Seungmin <rnsdls1997@gmail.com>
2026-01-20 02:49:02 +0900

    feat: 어노테이션 재귀 탐색 유틸 추가

---
+++ src/main/java/org/example/framework/util/AnnotationUtils.java
@@ -0,0 +1,42 @@
+package org.example.framework.util;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+import java.util.HashSet;
+import java.util.Set;
+
+public final class AnnotationUtils {
+    private AnnotationUtils() {}
+
+    /**
+     * 대상 클래스에 특정 어노테이션이 직접 또는 메타 어노테이션으로
+     * 존재하는지 검사한다.
+     */
+    public static boolean hasAnnotation(AnnotatedElement element, Class<? extends Annotation> annotationType) {
+        return hasAnnotation(element.getAnnotations(), annotationType, new HashSet<>());
+    }
+
+    private static boolean hasAnnotation(
+            Annotation[] annotations, Class<? extends Annotation> targetType, Set<Class<? extends Annotation>> visited
+    ) {
+        for (Annotation annotation : annotations) {
+            Class<? extends Annotation> currentType = annotation.annotationType();
+
+            // 순환 방지
+            if (!visited.add(currentType)) {
+                continue;
+            }
+
+            // 직접 매칭
+            if (currentType.equals(targetType)) {
+                return true;
+            }
+
+            // 메타 어노테이션 재귀 탐색
+            if (hasAnnotation(currentType.getAnnotations(), targetType, visited)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
