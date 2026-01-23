7988f6ea8b4d0e5f9d001d9d323ed547ceaaa7ca
Seungmin <rnsdls1997@gmail.com>
2026-01-23 22:43:47 +0900

    feat: WasInfrastructure 추가

---
+++ src/main/java/org/example/server/was/WasInfrastructure.java
@@ -0,0 +1,58 @@
+package org.example.server.was;
+
+import org.example.framework.was.adapter.DefaultServletAdapter;
+import org.example.framework.was.connector.Connector;
+import org.example.framework.was.container.Servlet;
+import org.example.framework.was.container.ServletContainer;
+import org.example.framework.was.protocol.HttpProtocolSelector;
+import org.example.framework.was.protocol.http.HttpProtocolHandlerFactory;
+import org.example.framework.was.server.Service;
+import org.example.server.LifeCycle;
+import org.example.server.config.SeungPringApplicationConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public final class WasInfrastructure implements LifeCycle {
+
+    private static final Logger log = LoggerFactory.getLogger(WasInfrastructure.class);
+
+    private final ExecutorService executor;
+    private final Connector connector;
+
+    public WasInfrastructure(SeungPringApplicationConfig config, Servlet servlet) {
+        this.executor = Executors.newFixedThreadPool(config.workerThreads());
+
+        HttpProtocolSelector selector = new HttpProtocolSelector();
+
+        Service service = new Service(new ServletContainer(servlet));
+
+        HttpProtocolHandlerFactory handlerFactory =
+                HttpProtocolHandlerFactory.create(new DefaultServletAdapter(service));
+
+        this.connector = new Connector(
+                config.port(),
+                executor,
+                selector,
+                handlerFactory,
+                service
+        );
+    }
+
+    @Override
+    public void start() throws Exception {
+        log.info("[WAS] Initializing server resources");
+        connector.start();
+        log.info("[WAS] Server initialization complete");
+    }
+
+    @Override
+    public void stop() throws Exception {
+        log.info("[WAS] Shutting down server");
+        connector.stop();
+        executor.shutdown();
+        log.info("[WAS] Server shutdown complete");
+    }
+}
