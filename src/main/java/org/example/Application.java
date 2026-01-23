8600e821bff92c37eddada470030f4dd6579bb81
Seungmin <rnsdls1997@gmail.com>
2026-01-23 22:46:06 +0900

    feat: Application에 ComponentScan 어노테이션 추가

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.framework.annotation.ComponentScan;
+import org.example.server.SeungPringApplication;
+
+@ComponentScan("org.example")
 public class Application {
     public static void main(String[] args) {
-
+        new SeungPringApplication().run(Application.class, args);
     }
 }
